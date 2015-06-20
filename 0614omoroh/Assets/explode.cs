8b8032ac1d6936b5de4c146012ad306a66a80459
今岡寛晴 <fine_nobu@FL1-60-238-174-137.kng.mesh.ad.jp>
2015-06-20 10:08:38 +0900

    0614omoroh

---
+++ 0614omoroh/Assets/explode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explode : MonoBehaviour {
+
+	float timer;
+	int waitingTime;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		waitingTime = 2;
+//		this.SendMessage ("Explode");
+	}
+	// Update is called once per frame
+	void Update () {
+	timer += Time.deltaTime;
+	if (timer > waitingTime) {
+		//Action
+			this.SendMessage ("Explode");
+			timer = 0;
+		}
+	}
+}
---
+++ 0614omoroh/Assets/test.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	public GameObject  g;
+	public GameObject  player;
+	public GameObject  bakudan;
+
+	public float x;
+	public float y;
+	public float z;
+
+
+	// Use this for initialization
+	void Start () {
+		g = GameObject.Find("test1");
+		Debug.Log( g.transform.position );
+//		Destroy(g, 15.0f);
+
+//		bakudan = GameObject.Find("bomb");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		g.transform.Rotate (0, 10, 0);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			player = GameObject.Find("ThirdPersonController");
+			x = player.transform.localPosition.x;
+			y = player.transform.localPosition.y;
+			z = player.transform.localPosition.z;
+//			Instantiate(Resources.Load("bomb"),new Vector3(x, y, z), Quaternion.identity) ;
+
+			GameObject bakudan = Instantiate(Resources.Load("bomb"),new Vector3(x, y, z), Quaternion.identity) as GameObject;
+			Destroy(bakudan, 3); 
+
+	//		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	//		sphere.transform.position = new Vector3(x, y, z);
+		}
+	}
+} 	 
